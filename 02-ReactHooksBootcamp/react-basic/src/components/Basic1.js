20cd381ca977fae303d606e04fb240cbc07634af
kakariko3 <kakariko01@gmail.com>
2021-02-14 19:59:30 +0900

    10. UseState(Object)

--- 02-ReactHooksBootcamp/react-basic/src/components/Basic1.js
+++ 02-ReactHooksBootcamp/react-basic/src/components/Basic1.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Basic1 = () => {
-    return (
-        <>
-            <h1>Hello React1</h1>
-            <h1>Hello React2</h1>
-        </>
-    )
+
+  const [product, setProducts] = useState({name: '', price: ''});
+
+  return (
+    <>
+      <form>
+        <input type='text' value={product.name}
+        onChange={evt => setProducts({...product, name: evt.target.value})} />
+
+        <input type='text' value={product.price}
+        onChange={evt => setProducts({...product, price: evt.target.value})} />  
+      </form>
+      <h3>Product name is {product.name}</h3>
+      <h3>Product price is {product.price}</h3>
+    </>
+  );
 }
 
 export default Basic1
