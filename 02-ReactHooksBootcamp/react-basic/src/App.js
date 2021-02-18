e5aecc995e330785e18ac3f9e7d99e9a60dda247
kakariko3 <kakariko01@gmail.com>
2021-02-18 17:28:52 +0900

    22. useReducer + useContext

--- 02-ReactHooksBootcamp/react-basic/src/App.js
+++ 02-ReactHooksBootcamp/react-basic/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 // import Basic2 from './components/Basic2';
@@ -6,11 +7,29 @@ import './App.css';
 // import ApiFetch from './components/ApiFetch';
 import AppContext from './contexts/AppContext';
 // import B from './components/B';
-import BasicReducer from './components/BasicReducer';
+// import BasicReducer from './components/BasicReducer';
+import CompB from './components/CompB';
+
+import {useReducer} from 'react';
+
+const initialState = 0;
+const reducer = (currentState, action) => {
+  switch (action) {
+    case 'add_1':
+      return currentState + 1;
+    case 'multiple_3':
+      return currentState * 3;
+    case 'reset':
+      return initialState;
+    default:
+      return currentState;
+  }
+}
 
 function App() {
+  const [count, dispatch] = useReducer(reducer, initialState);
   return (
-    <AppContext.Provider value={'value from App.js'}>
+    <AppContext.Provider value={{countProvided: count, dispatchProvided: dispatch}}>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -19,7 +38,9 @@ function App() {
           {/* <TimerContainer /> */}
           {/* <ApiFetch /> */}
           {/* <B /> */}
-          <BasicReducer />
+          {/* <BasicReducer /> */}
+          Count {count}
+          <CompB />
           
         </header>
       </div>
